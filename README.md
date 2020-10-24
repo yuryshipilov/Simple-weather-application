07c94f27d95e2530821db23ee6a0420e81e2a29d
Yury <yurshipilov@gmail.com>
2020-10-24 20:45:15 +0400

    Simple weather application

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a Weather Application created using Python 3.6.9 / Django 3.1.2
+
+
+How to starting this application in local server?
+
+Bash : python manage.py runserver
---
+++ weather/views.py
@@ -0,0 +1,35 @@
+import requests
+from django.shortcuts import render
+from weather.models import City
+from weather.forms import CityForm
+
+def index(request):
+    appid = '476ea54f46fff726ec39416809d4b6b1'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form' : form}
+
+    return render(request, 'index.html', context)
+
+
+
